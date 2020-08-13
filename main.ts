e3bbf3b4ef0f174dfc8b688057deea11034ea4b1
Andrew <afmcdnl@gmail.com>
2020-08-12 21:59:46 -0400

    Update to move in the array with the A and Buttons

--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
 input.onButtonPressed(Button.A, function () {
     index += 1
+    if (index > list.length - 1) {
+        index = list.length - 1
+    }
 })
 input.onButtonPressed(Button.B, function () {
     index += -1
+    if (index < 0) {
+        index = 0
+    }
 })
 let suggested_number = 0
-let list = [0]
-let min = 1
-let max = 5
 let index = 0
+let list: number[] = []
+list = [0]
+let min = 1
+let max = 4
+index = 0
 list.shift()
 basic.showIcon(IconNames.Heart)
-while (list.length < max - (min + 1)) {
+while (list.length < max - min + 1) {
     suggested_number = randint(min, max)
     if (list.indexOf(suggested_number) < 0) {
         list.push(suggested_number)
